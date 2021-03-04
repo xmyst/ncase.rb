5b9142f080578ce5d1c85d89863cbd2ee2395708
Stas Miasnikoŭ <miasnikou@yandex.by>
2021-03-05 00:52:49 +0100

    test: refactor and extend the unittests

--- test/ncase/words_test.rb
+++ test/ncase/words_test.rb
@@ -5,30 +5,38 @@ require "ncase"
 
 module Ncase
   class WordsTest < Minitest::Test
+    parallelize_me!
+
     TEST_STRING = "this is a test string"
 
-    def test_guess_separator
+    def test_new_no_separator
       {
-        "this is-a test_string" => "This Is-a Test_string",
+        "this is-a_testString"  => "This Is-a_teststring",
         "this-is-a-test_string" => "This Is A Test_string",
         "this_is-a_test-string" => "This_is A_test String",
-        "this_is_a_test-string" => "This Is A Test-string",
+        "this-is_a_test_string" => "This-is A Test String",
         "ThisIsATestString"     => "This Is A Test String"
-      }.each do |s, t|
-        assert_equal t, Words.new(s).title_case
+      }.each do |x, y|
+        assert_equal y, Words.new(x).title_case
       end
     end
 
-    def test_separator_string
-      s = "this is a test string"
-      t = "His is a Es sRing"
-      assert_equal t, Words.new(s, separator: "t").camel_case
+    def test_new_separator_string
+      {
+        "is" => "th_ _ a test string",
+        ""   => "t_h_i_s_ _i_s_ _a_ _t_e_s_t_ _s_t_r_i_n_g",
+      }.each do |x, y|
+        assert_equal y, Words.new(TEST_STRING, separator: x).snake_case
+      end
     end
 
-    def test_separator_regex
-      s = "this is a test string"
-      t = "HS S a Es sRNg"
-      assert_equal t, Words.new(s, separator: /t|i/).camel_case
+    def test_new_separator_regexp
+      {
+        /is?/ => "th_ _ a test str_ng",
+        //    => "t_h_i_s_ _i_s_ _a_ _t_e_s_t_ _s_t_r_i_n_g",
+      }.each do |x, y|
+        assert_equal y, Words.new(TEST_STRING, separator: x).snake_case
+      end
     end
 
     def test_non_random_cases
@@ -44,8 +52,8 @@ module Ncase
         upper_snake_case: "THIS_IS_A_TEST_STRING",
         title_case:       "This Is A Test String",
         inver_title_case: "tHIS iS a tEST sTRING"
-      }.each do |m, s|
-        assert_equal s, (w.__send__ m)
+      }.each do |x, y|
+        assert_equal y, w.__send__(x)
       end
     end
 
@@ -57,7 +65,7 @@ module Ncase
       assert TEST_STRING.casecmp?(r1)
       assert TEST_STRING.casecmp?(r2)
       assert r1.casecmp?(r2)
-      refute r1 == r2
+      refute_equal r1, r2
     end
   end
 end
