f52a33570c36d0e3ab1f30f46df8563425654772
Stas Miasnikoŭ <miasnikou@yandex.by>
2021-10-16 22:06:37 +0200

    test: refactor the unit tests to follow AAA

--- test/ncase/words_test.rb
+++ test/ncase/words_test.rb
@@ -7,64 +7,118 @@ module Ncase
   class WordsTest < Minitest::Test
     parallelize_me!
 
-    TEST_STRING = "this is a test string"
-
-    def test_new_no_separator
-      {
-        "this is-a_testString"  => "This Is-a_teststring",
-        "this-is-a-test_string" => "This Is A Test_string",
-        "this_is-a_test-string" => "This_is A_test String",
-        "this-is_a_test_string" => "This-is A Test String",
-        "ThisIsATestString"     => "This Is A Test String"
-      }.each do |x, y|
-        assert_equal y, Words.new(x).title_case
-      end
+    def test_new_no_separator_guess_space
+      str = "this is-a_testString"
+      act = Words.new(str).title_case
+      assert_equal "This Is-a_teststring", act
     end
 
+    def test_new_no_separator_guess_hyphen
+      str = "this-is-a-test_string"
+      act = Words.new(str).title_case
+      assert_equal "This Is A Test_string", act
+    end
+
+    def test_new_no_separator_guess_hyphen_over_underscore
+      str = "this_is-a_test-string"
+      act = Words.new(str).title_case
+      assert_equal "This_is A_test String", act
+    end
+
+    def test_new_no_separator_guess_underscore
+      str = "this-is_a_test_string"
+      act = Words.new(str).title_case
+      assert_equal "This-is A Test String", act
+    end
+
+    def test_new_no_separator_guess_case
+      str = "ThisIsATestString"
+      act = Words.new(str).title_case
+      assert_equal "This Is A Test String", act
+    end
+
+    TEST_STRING = "this is a test string"
+
     def test_new_separator_string
-      {
-        "is" => "th_ _ a test string",
-        ""   => "t_h_i_s_ _i_s_ _a_ _t_e_s_t_ _s_t_r_i_n_g"
-      }.each do |x, y|
-        assert_equal y, Words.new(TEST_STRING, separator: x).snake_case
-      end
+      sep = "is"
+      act = Words.new(TEST_STRING, separator: sep).snake_case
+      assert_equal "th_ _ a test string", act
     end
 
     def test_new_separator_regexp
-      {
-        /is?/ => "th_ _ a test str_ng",
-        //    => "t_h_i_s_ _i_s_ _a_ _t_e_s_t_ _s_t_r_i_n_g"
-      }.each do |x, y|
-        assert_equal y, Words.new(TEST_STRING, separator: x).snake_case
-      end
+      sep = /is?/
+      act = Words.new(TEST_STRING, separator: sep).snake_case
+      assert_equal "th_ _ a test str_ng", act
     end
 
-    def test_non_random_cases
+    def test_camel_case
       w = Words.new(TEST_STRING)
-      {
-        camel_case:       "thisIsATestString",
-        pascal_case:      "ThisIsATestString",
-        kebab_case:       "this-is-a-test-string",
-        upper_kebab_case: "THIS-IS-A-TEST-STRING",
-        lower_case:       "this is a test string",
-        upper_case:       "THIS IS A TEST STRING",
-        snake_case:       "this_is_a_test_string",
-        upper_snake_case: "THIS_IS_A_TEST_STRING",
-        title_case:       "This Is A Test String",
-        inver_title_case: "tHIS iS a tEST sTRING"
-      }.each do |x, y|
-        assert_equal y, w.__send__(x)
-      end
+      act = w.camel_case
+      assert_equal "thisIsATestString", act
+    end
+
+    def test_pascal_case
+      w = Words.new(TEST_STRING)
+      act = w.pascal_case
+      assert_equal "ThisIsATestString", act
+    end
+
+    def test_kebab_case
+      w = Words.new(TEST_STRING)
+      act = w.kebab_case
+      assert_equal "this-is-a-test-string", act
+    end
+
+    def test_upper_kebab_case
+      w = Words.new(TEST_STRING)
+      act = w.upper_kebab_case
+      assert_equal "THIS-IS-A-TEST-STRING", act
+    end
+
+    def test_lower_case
+      w = Words.new(TEST_STRING)
+      act = w.lower_case
+      assert_equal "this is a test string", act
+    end
+
+    def test_upper_case
+      w = Words.new(TEST_STRING)
+      act = w.upper_case
+      assert_equal "THIS IS A TEST STRING", act
+    end
+
+    def test_snake_case
+      w = Words.new(TEST_STRING)
+      act = w.snake_case
+      assert_equal "this_is_a_test_string", act
+    end
+
+    def test_upper_snake_case
+      w = Words.new(TEST_STRING)
+      act = w.upper_snake_case
+      assert_equal "THIS_IS_A_TEST_STRING", act
+    end
+
+    def test_title_case
+      w = Words.new(TEST_STRING)
+      act = w.title_case
+      assert_equal "This Is A Test String", act
+    end
+
+    def test_inver_title_case
+      w = Words.new(TEST_STRING)
+      act = w.inver_title_case
+      assert_equal "tHIS iS a tEST sTRING", act
     end
 
     def test_random_case
       w = Words.new(TEST_STRING)
-      r1 = w.random_case
-      r2 = w.random_case
 
-      assert TEST_STRING.casecmp?(r1)
-      assert TEST_STRING.casecmp?(r2)
-      refute_equal r1, r2
+      act1 = w.random_case
+      act2 = w.random_case
+
+      assert act1.casecmp?(act2)
+      refute_equal act1, act2
     end
   end
 end
